2dbf180cf502a4886fda783e8f6c8d486fab58f4
Shigema <tsubakyun4869@gmail.com>
2023-08-05 17:20:17 +0900

    [add] 壁・プレイヤーに接触したら反転し、床から落ちない敵キャラのスクリプト

--- Assets/Scripts/EnemyMove.cs
+++ Assets/Scripts/EnemyMove.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -10,10 +11,13 @@ public class EnemyMove : MonoBehaviour
     //[SerializeField, Tooltip("範囲")] float _moveRange = 5.0f;
     [SerializeField, Tooltip("速度")] float _moveSpeed = 0.5f;
     Vector3 _scale = default;
-    [SerializeField] LayerMask _tile;
+    [SerializeField] LayerMask _wall;
+    [SerializeField] LayerMask _ground;
     Rigidbody2D _rb = default;
     Vector2 _moveDirection = new Vector2(1, 0);
     [SerializeField] Vector2 _lineForWall = new Vector2(1, -0.5f);
+    [SerializeField] Vector2 _lineForGround = new Vector2(1, -1f);
+    [Tooltip("地面に接したら真")] bool _canMove = false; 
 
 
 
@@ -24,13 +28,16 @@ public class EnemyMove : MonoBehaviour
         //_scale = GameObject.Find(this.gameObject.name + "/Chara").transform.localScale;
         // 一度だけ、左向きを右向きに修正
         _scale.x = -_scale.x;
-        transform.localScale = _scale; 
+        transform.localScale = _scale;
+        _canMove = false;
     }
 
 
     void Update()
     {
-        Move();
+        CanMoveOnce(); 
+        if (_canMove)
+            Move();
     }
 
     
@@ -38,20 +45,43 @@ public class EnemyMove : MonoBehaviour
     void Move()
     {
         Vector2 start = this.transform.position;
+        // 壁の判定 
         Debug.DrawLine(start, start + _lineForWall);
-        RaycastHit2D hit = Physics2D.Linecast(start, start + _lineForWall, _tile);
+        RaycastHit2D hit_wall = Physics2D.Linecast(start, start + _lineForWall, _wall);
+        // 床の判定 
+        Debug.DrawLine(start, start + _lineForGround);
+        RaycastHit2D hit_ground = Physics2D.Linecast(start, start + _lineForGround, _ground);
         Vector2 velo = Vector2.zero;    // velo は速度ベクトル 
 
-        if (hit.collider)
+        if (hit_wall || !hit_ground)
         {
             _moveDirection = -_moveDirection;
             _lineForWall.x = -_lineForWall.x;
+            _lineForGround.x = -_lineForGround.x; 
             _scale.x = -_scale.x;
         }
-
         transform.localScale = _scale; 
         velo = _moveDirection.normalized * _moveSpeed;
         velo.y = _rb.velocity.y;
         _rb.velocity = velo;
+    }
+
+    /// <summary>
+    /// 出現時に空中にいるとき、左右にぶれるため、
+    /// 地面に一度触れるまでは、Move()を実行させない
+    /// </summary>
+    void CanMoveOnce()
+    {
+        // 地面に接したらラインを引かない  
+        if (!_canMove)
+        {
+            Vector2 start = this.transform.position;
+            Debug.DrawLine(start, start + new Vector2(0, -2f));
+            RaycastHit2D hit_ground = Physics2D.Linecast(start, start + new Vector2(0, -2f), _ground);
+            if (hit_ground)
+            {
+                _canMove = true;
+            }
+        }
     }
 }
