494a8463326f6eafb41b3a2120671302a24da476
Shigema <tsubakyun4869@gmail.com>
2023-08-08 12:18:50 +0900

    [update] ランキング機能

--- Assets/Scripts/UseScoreSaveData.cs
+++ Assets/Scripts/UseScoreSaveData.cs
@@ -1,92 +1,112 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class UseScoreSaveData : MonoBehaviour
+/// <summary>
+/// ランキング機能
+/// プレイヤー名が被ったら、データが上書きされる
+/// スコアが重複したら古いデータ順に、降順になる
+/// </summary>
+public class UseSaveData : MonoBehaviour
 {
-    [SerializeField] InputField[] inputFields;
-    SaveManager saveManager = new SaveManager();
-    string SaveDirPath;
+    [SerializeField] InputField[] _inputFields;
 
-    string[] _existFiles = null;
+    SaveManager _saveManager = new SaveManager();
+    string _saveDirPath;
+    // Directory.GetFiles が string[] を返す 
+    [Tooltip("ソートされていないテキストデータ")] string[] _existFiles = null;
+    [Tooltip("ソート前の複数のスコアが入ったリスト")] List<int> numberList = new List<int>();
+    [Tooltip("ソート済みの複数のスコアが入ったリスト")] List<int> sortedList = new List<int>();
+    [Tooltip("何位")] int _rank = 0;
     [SerializeField] Text _text = default;
-
-    List<int> numberList = new List<int>();
 
     private void Awake()
     {
-        // 略 
-        SaveDirPath = Application.persistentDataPath;
-        Debug.Log(SaveDirPath);
+        // 保存先 
+        _saveDirPath = Application.persistentDataPath;
+        Debug.Log(_saveDirPath);
     }
 
     private void Update()
     {
-        // 略 
+        // データを作って保存  
         if (Input.GetKeyDown(KeyCode.P))
         {
-            ScoreSaveData scoreSaveData = saveManager.CreateSaveData(inputFields[0].text, int.Parse(inputFields[1].text), int.Parse(inputFields[2].text), int.Parse(inputFields[3].text));
-            saveManager.DataSave(scoreSaveData, SaveDirPath, $"スコア{inputFields[1].text}のデータ.data");
-            Debug.Log($"{scoreSaveData._playerName} _playerName : {scoreSaveData._score} _score {scoreSaveData._killCount} _killCount {scoreSaveData._remainingHp} _remainingHp    {SaveDirPath} : SaveDirPath");
+            string playerName = _inputFields[0].text;
+            ScoreSaveData sdata = _saveManager.CreateSaveData(playerName, int.Parse(_inputFields[1].text), int.Parse(_inputFields[2].text), int.Parse(_inputFields[3].text));
+            _saveManager.DataSave(sdata, _saveDirPath, $"{playerName}のデータ.data"); 
         }
-        if (Input.GetKeyDown(KeyCode.O))
-        {
-            // スコアの数値 検索でデータを呼び出せる 
-            ScoreSaveData scoreSaveData = saveManager.DataLoad(SaveDirPath, $"スコア{inputFields[1].text}のデータ.data");
-            Debug.Log($"{scoreSaveData._playerName} _playerName : {scoreSaveData._score} _score {scoreSaveData._killCount} _killCount {scoreSaveData._remainingHp} _remainingHp");
-        }
-
-
         if (Input.GetKeyDown(KeyCode.S))
         {
             numberList.Clear();
-            // フォルダ内の全てのファイルの、検索のテスト 
-            _existFiles = Directory.GetFiles(SaveDirPath, "*");
-            foreach (string str in _existFiles)
-            {
-                ScoreSaveData scoreSaveData = saveManager.DataLoad(SaveDirPath, str);
-                numberList.Add(scoreSaveData._score);
-            }
-            foreach (int chr in numberList)
-                Debug.Log($"numberList : {chr}");
-        }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            // スコア順に並び替え 
-            SortScore();
+            sortedList.Clear();
+            _rank = 0;
+            _text.text = null;
+            SortAndAdd();
+            ShowText();
+            Delete();
         }
     }
-
-
-    void SortScore()
+    /// <summary>
+    /// テキストデータの数が11以上になったら、削除 
+    /// </summary>
+    void Delete()
     {
-        _text.text = null;
-        // リストを降順ソート
-        var sortedList = numberList.OrderByDescending(i => i).ToList();
+        if (sortedList.Count >= 11)
+        {
+            for (var i = 10; i < sortedList.Count; i++)
+            {
+                for (var j = 0; j < _existFiles.Length; j++)
+                {
+                    ScoreSaveData sdata = _saveManager.DataLoad(_saveDirPath, _existFiles[j]);
+                    // スコアリストの上から順に、テキストデータとスコアが一致したら削除する 
+                    if (sortedList[i] == sdata._score)
+                    {
+                        File.Delete(Path.Combine(_saveDirPath, _existFiles[j]));
+                        Debug.LogWarning($"{_existFiles[j]} を削除しました。");
+                        sortedList.RemoveAt(i);
+                    }
+                }
+            }
+        }
+    }
+    /// <summary>
+    /// 全検索、ソート、追加 
+    /// </summary>
+    void SortAndAdd()
+    {
+        // フォルダ内の全てのファイルの検索  
+        _existFiles = Directory.GetFiles(_saveDirPath, "*");
+        foreach (string str in _existFiles)
+        {
+            ScoreSaveData sdata = _saveManager.DataLoad(_saveDirPath, str);
+            // 値を重複させない 
+            if (!numberList.Contains(sdata._score))
+                numberList.Add(sdata._score);
+            // リストを降順ソート
+            sortedList = numberList.OrderByDescending(i => i).ToList();
+        }
         Debug.LogWarning(string.Join(", ", sortedList));
+    }
+    /// <summary>
+    /// テキスト表示 
+    /// </summary>
+    void ShowText()
+    {
+        // スコアリストの上から順に、テキストデータとスコアが一致したらテキスト表示する 
         for (var i = 0; i < sortedList.Count; i++)
         {
-            ScoreSaveData sdata = saveManager.DataLoad(SaveDirPath, $"スコア{sortedList[i]}のデータ.data");
-            _text.text = _text.text + $"{i + 1}位：プレイヤー名 : {sdata._playerName} スコア : {sdata._score} \n";
-            Delete(sortedList.Count, sortedList); 
-        }
-    }
-
-    /// <summary>
-    /// テキストデータの数が6以上になったら、削除 
-    /// </summary>
-    /// <param name="num">リストの要素数</param>
-    /// <param name="list">スコアの値が入ったリスト</param>
-    void Delete(int num, List<int> list)
-    {
-        if (num >= 6)
-        {
-            File.Delete(Path.Combine(SaveDirPath, $"スコア{list[5]}のデータ.data"));
-            Debug.LogWarning($"スコア{list[5]}のデータ.data を削除しました。");
-            list.RemoveAt(5);
+            for (var j = 0; j < _existFiles.Length; j++)
+            {
+                ScoreSaveData sdata = _saveManager.DataLoad(_saveDirPath, _existFiles[j]);
+                if (sortedList[i] == sdata._score)
+                {
+                    _rank++;
+                    _text.text = _text.text + $"{_rank}位：名前 : {sdata._playerName} スコア : {sdata._score} \n";
+                }
+            }
         }
     }
 }
