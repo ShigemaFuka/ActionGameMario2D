932cc8277ef1e25946efeb8b056825a7546f44e8
Shigema <tsubakyun4869@gmail.com>
2023-10-01 10:08:57 +0900

    [add] 落下時のSE

--- Assets/Scripts/DeadZoneController.cs
+++ Assets/Scripts/DeadZoneController.cs
@@ -9,30 +9,23 @@ public class DeadZoneController : MonoBehaviour
 {
     [SerializeField] GameManager _gameManager = default;
     [SerializeField] EnemyGenerator _enemyGenerator = default;
-           
-    //void OnCollisionEnter2D(Collision2D coll)
-    //{
-        //if (coll.gameObject.CompareTag("Player"))
-        //{
-        //    _gameManager.GameOver();
-        //}
-        //else if (coll.gameObject.CompareTag("Enemy"))
-        //{
-        //    _enemyGenerator.Collect(_enemyGenerator.PrefabQueue, coll.gameObject);
-        //    _enemyGenerator.CharaCount--;
-        //}
-    //}
-    private void OnTriggerEnter2D(Collider2D coll)
+    [SerializeField, Tooltip("ÉvÉåÉCÉÑÅ[ê⁄êGéûÇÃSE")] AudioSource _audioSource = default;
+
+    void OnEnable()
+    {
+        _audioSource.Stop();
+    }
+    void OnTriggerEnter2D(Collider2D coll)
     {
         if(!_gameManager) _gameManager = FindAnyObjectByType<GameManager>();
         if (coll.gameObject.CompareTag("Player"))
         {
+            _audioSource.PlayOneShot(_audioSource.clip);
             _gameManager.GameOver();
         }
         else if (coll.gameObject.CompareTag("Enemy"))
         {
             _enemyGenerator.Collect(_enemyGenerator.PrefabQueue, coll.gameObject);
-            _enemyGenerator.CharaCount--;
         }
     }
 }
